61e9e2ef6367bbda72001838219806286511cb27
Shilish <shilishvatsin@gmail.com>
2021-10-21 00:10:57 +0530

    Added a SAY command that can send messages or embeds

--- commands/Admin-Only/say.js
+++ commands/Admin-Only/say.js
@@ -11,15 +11,17 @@ exports.default = {
         var message = _a.message, channel = _a.channel, args = _a.args;
         try {
             var json = JSON.parse(args.join(' '));
-            if (!(json === null || json === void 0 ? void 0 : json.embeds) && !(json === null || json === void 0 ? void 0 : json.embeds.description)) {
-                return '`JSON Data` is invalid as no embed exists. Please visit [https://discohook.org/] for designing your embed';
-            }
+            // if (!json?.embeds && !json?.embeds.description) {
+            // 	return '`JSON Data` is invalid as no embed exists. Please visit [https://discohook.org/] for designing your embed';
+            // }
             channel.send(json).catch(function (err1) {
-                message.reply("`JSON Data` is invalid. Please copy `JSON Data` from [https://discohook.org/]");
+                message.reply('`JSON Data` is invalid. Please copy `JSON Data` from [https://discohook.org/]');
             });
-            if (message.deletable) {
-                message.delete();
-            }
+            setTimeout(function () {
+                if (message.deletable) {
+                    message.delete();
+                }
+            }, 2500);
         }
         catch (err) {
             return "`JSON Data` is invalid. Please visit [https://discohook.org/] for designing your embed";
--- commands/Admin-Only/say.ts
+++ commands/Admin-Only/say.ts
@@ -13,19 +13,21 @@ export default {
 		try {
 			const json = JSON.parse(args.join(' '));
 
-			if (!json?.embeds && !json?.embeds.description) {
-				return '`JSON Data` is invalid as no embed exists. Please visit [https://discohook.org/] for designing your embed';
-			}
+			// if (!json?.embeds && !json?.embeds.description) {
+			// 	return '`JSON Data` is invalid as no embed exists. Please visit [https://discohook.org/] for designing your embed';
+			// }
 
 			channel.send(json).catch((err1) => {
 				message.reply(
-					`\`JSON Data\` is invalid. Please copy \`JSON Data\` from [https://discohook.org/]`
+					'`JSON Data` is invalid. Please copy `JSON Data` from [https://discohook.org/]'
 				);
 			});
 
-			if (message.deletable) {
-				message.delete();
-			}
+			setTimeout(() => {
+				if (message.deletable) {
+					message.delete();
+				}
+			}, 2500);
 		} catch (err) {
 			return `\`JSON Data\` is invalid. Please visit [https://discohook.org/] for designing your embed`;
 		}
