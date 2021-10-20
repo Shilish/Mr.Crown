0a8fcaccdbf5191cc005de289dcd92c19bb6b43e
Shilish <shilishvatsin@gmail.com>
2021-10-20 23:48:30 +0530

    Added ability for users to send embeds

---
+++ commands/Admin-Only/embed.js
@@ -0,0 +1,25 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.default = {
+    category: 'Admin Only',
+    description: 'Send a embed based on provided JSON data',
+    requiredPermissions: ['ADMINISTRATOR'],
+    slash: false,
+    minArgs: 1,
+    expectedArgs: '<JSON Data>',
+    callback: function (_a) {
+        var message = _a.message, channel = _a.channel, args = _a.args;
+        try {
+            var json = JSON.parse(args.join(' '));
+            if (!(json === null || json === void 0 ? void 0 : json.embeds) && !(json === null || json === void 0 ? void 0 : json.embeds.description)) {
+                return '`JSON Data` is invalid as no embed exists. Please visit [https://discohook.org/] for designing your embed';
+            }
+            channel.send(json).catch(function (err1) {
+                message.reply("`JSON Data` is invalid. Please copy `JSON Data` from [https://discohook.org/]");
+            });
+        }
+        catch (err) {
+            return "`JSON Data` is invalid. Please visit [https://discohook.org/] for designing your embed";
+        }
+    },
+};
---
+++ commands/Admin-Only/embed.ts
@@ -0,0 +1,31 @@
+import { ICommand } from 'wokcommands';
+
+export default {
+	category: 'Admin Only',
+	description: 'Send a embed based on provided JSON data',
+	requiredPermissions: ['ADMINISTRATOR'],
+	slash: false,
+
+	minArgs: 1,
+	expectedArgs: '<JSON Data>',
+
+	callback: ({ message, channel, args }) => {
+		try {
+			const json = JSON.parse(args.join(' '));
+
+			if (!json?.embeds && !json?.embeds.description) {
+				return '`JSON Data` is invalid as no embed exists. Please visit [https://discohook.org/] for designing your embed';
+			}
+
+			channel.send(json).catch((err1) => {
+				message.reply(
+					`\`JSON Data\` is invalid. Please copy \`JSON Data\` from [https://discohook.org/]`
+				);
+			});
+		} catch (err) {
+			return `\`JSON Data\` is invalid. Please visit [https://discohook.org/] for designing your embed`;
+		}
+	},
+} as ICommand;
+
+//Shilish "Accio" Vatsin
